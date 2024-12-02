64175a7555cd4a3dfe9056b96b88815fddb9892d
MartinHurnak <hurnak.m@gmail.com>
2024-12-02 20:50:16 +0100

    Prepare directory structure for upcoming days

---
+++ 11/1.py
@@ -0,0 +1,24 @@
+import os
+
+dirname = os.path.dirname(__file__)
+
+
+def test_and_run():
+    test_result = main(os.path.join(dirname, "test.txt"))
+    if test_result != EXPECTED_TEST_RESULT:
+        print("Result doesn't match test, expected: {EXPECTED_TEST_RESULT}, actual")
+
+    print("Test OK, Result:", main(os.path.join(dirname, "input.txt")))
+
+
+def main(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+
+    # TODO implement me
+
+    return None
+
+
+EXPECTED_TEST_RESULT = 0  # TODO change based on test
+test_and_run()
---
+++ dirmaker.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+for i in range(3, 26):
+    dirname = str(i)
+    os.makedirs(dirname, exist_ok=True)
+    for filename in ["1.py", "2.py"]:
+        shutil.copy("template.py", os.path.join(dirname, filename))
+    for filename in ["test.txt", "input.txt"]:
+        with open(os.path.join(dirname, filename), "w"):
+            pass
