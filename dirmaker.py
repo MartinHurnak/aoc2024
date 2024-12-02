102d9d3e05082b26451c08ed3b57c94418aad460
MartinHurnak <hurnak.m@gmail.com>
2024-12-02 20:53:38 +0100

    Fix directory names

--- dirmaker.py
+++ dirmaker.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 for i in range(3, 26):
-    dirname = str(i)
+    dirname = str(i).zfill(2)
     os.makedirs(dirname, exist_ok=True)
     for filename in ["1.py", "2.py"]:
         shutil.copy("template.py", os.path.join(dirname, filename))
