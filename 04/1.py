676a2b9affddae5733e103e5dfb5d57207b60cde
MartinHurnak <hurnak.m@gmail.com>
2024-12-06 14:36:23 +0100

    Day 4

--- 04/1.py
+++ 04/1.py
@@ -6,19 +6,37 @@ dirname = os.path.dirname(__file__)
 def test_and_run():
     test_result = main(os.path.join(dirname, "test.txt"))
     if test_result != EXPECTED_TEST_RESULT:
-        print(f"Result doesn't match test, expected: {EXPECTED_TEST_RESULT}, actual")
+        print(f"Result doesn't match test, expected: {EXPECTED_TEST_RESULT}, actual {test_result}")
     else:
         print("Test OK, Result:", main(os.path.join(dirname, "input.txt")))
 
 
+NEEDLE = "XMAS"
+
+def search(map, y, x, dy, dx, depth=0):
+    if depth >= len(NEEDLE):
+        return True
+    if not 0 <= y < len(map):
+        return False
+    if not 0 <= x < len(map[y]):
+        return False
+    if map[y][x] == NEEDLE[depth]:
+        return search(map, y + dy, x + dx, dy, dx, depth + 1)
+
+
 def main(filename):
     with open(filename) as f:
-        lines = f.readlines()
+        map = f.readlines()
 
-    # TODO implement me
+    sum = 0
+    for y, row in enumerate(map):
+        for x, column in enumerate(row):
+            for dy in (-1, 0, 1):
+                for dx in (-1, 0, 1):
+                    if search(map, y, x, dy, dx):
+                        sum += 1
 
-    return None
+    return sum
 
-
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+EXPECTED_TEST_RESULT = 18
 test_and_run()
--- 04/2.py
+++ 04/2.py
@@ -4,21 +4,42 @@ dirname = os.path.dirname(__file__)
 
 
 def test_and_run():
-    test_result = main(os.path.join(dirname, "test.txt"))
+    test_result = main(os.path.join(dirname, "test2.txt"))
     if test_result != EXPECTED_TEST_RESULT:
-        print(f"Result doesn't match test, expected: {EXPECTED_TEST_RESULT}, actual")
+        print(
+            f"Result doesn't match test, expected: {EXPECTED_TEST_RESULT}, actual {test_result}"
+        )
     else:
         print("Test OK, Result:", main(os.path.join(dirname, "input.txt")))
 
 
+def check_diagonal(map, y, x, dy1, dx1, dy2, dx2):
+    if not 0 <= y + dy1 < len(map) or not 0 <= y + dy2 < len(map):
+        return False
+    if not 0 <= x + dx1 < len(map[y]) or not 0 <= x + dx2 < len(map[y]):
+        return False
+    if map[y + dy1][x + dx1] == "S" and map[y + dy2][x + dx2] == "M":
+        return True
+    if map[y + dy1][x + dx1] == "M" and map[y + dy2][x + dx2] == "S":
+        return True
+    return False
+
+
 def main(filename):
     with open(filename) as f:
-        lines = f.readlines()
+        map = f.readlines()
 
-    # TODO implement me
+    sum = 0
+    for y, row in enumerate(map):
+        for x, column in enumerate(row):
+            if column == "A":
+                if check_diagonal(map, y, x, -1, -1, 1, 1) and check_diagonal(
+                    map, y, x, -1, 1, 1, -1
+                ):
+                    sum += 1
 
-    return None
+    return sum
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+EXPECTED_TEST_RESULT = 9
 test_and_run()
