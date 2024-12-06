014c76ef3a418f6df0d431cdce8ae984bcf88d22
MartinHurnak <hurnak.m@gmail.com>
2024-12-06 12:55:59 +0100

    Day 3

--- 03/1.py
+++ 03/1.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 dirname = os.path.dirname(__file__)
 
@@ -6,19 +7,22 @@ dirname = os.path.dirname(__file__)
 def test_and_run():
     test_result = main(os.path.join(dirname, "test.txt"))
     if test_result != EXPECTED_TEST_RESULT:
-        print("Result doesn't match test, expected: {EXPECTED_TEST_RESULT}, actual")
-
-    print("Test OK, Result:", main(os.path.join(dirname, "input.txt")))
+        print(f"Result doesn't match test, expected: {EXPECTED_TEST_RESULT}, actual")
+    else:
+        print("Test OK, Result:", main(os.path.join(dirname, "input.txt")))
 
 
 def main(filename):
     with open(filename) as f:
         lines = f.readlines()
 
-    # TODO implement me
+    sum = 0
+    for line in lines:
+        for match in re.finditer(r"mul\((\d+),(\d+)\)", line):
+            a, b = (int(number) for number in match.groups())
+            sum += a * b
+    return sum
 
-    return None
 
-
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+EXPECTED_TEST_RESULT = 161
 test_and_run()
