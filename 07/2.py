cb5e3c3ff235be13967cbc5c139c3ee91b4a3df0
MartinHurnak <hurnak.m@gmail.com>
2024-12-07 11:40:32 +0100

    Day 7

--- 07/2.py
+++ 07/2.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
 
@@ -7,14 +8,42 @@ dirname = os.path.dirname(__file__)
 testfile = os.path.join(dirname, "test.txt")
 inputfile = os.path.join(dirname, "input.txt")
 
+
+def concat(a, b):
+    return int(f"{a}{b}")
+
+
+def validate_operators(expected, operands, tmp_result):
+    if tmp_result > expected:
+        # early stop optimization, cannot be fixed by applying any operator
+        return False
+    if len(operands) == 0:
+        return tmp_result == expected
+    else:
+        return (
+            validate_operators(expected, operands[1:], tmp_result + operands[0])
+            or validate_operators(expected, operands[1:], tmp_result * operands[0])
+            or validate_operators(
+                expected, operands[1:], concat(tmp_result, operands[0])
+            )
+        )
+
+
 def main(filename):
     with open(filename) as f:
         lines = f.readlines()
 
-    # TODO implement me
+    sum = 0
+    for line in lines:
+        result, operands = line.split(": ")
+        result = int(result)
+        operands = [int(operand) for operand in operands.split()]
 
-    return None
+        if validate_operators(result, operands[1:], operands[0]):
+            sum += result
+
+    return sum
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+EXPECTED_TEST_RESULT = 11387
 test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
