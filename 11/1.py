0d5efd16b41f3c7874c425195428497b434ebe86
MartinHurnak <hurnak.m@gmail.com>
2024-12-11 11:16:34 +0100

    Day 11

--- 11/1.py
+++ 11/1.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import math
+
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
 
@@ -7,14 +9,35 @@ dirname = os.path.dirname(__file__)
 testfile = os.path.join(dirname, "test.txt")
 inputfile = os.path.join(dirname, "input.txt")
 
+
+def digits(x):
+    return int(math.log10(x)) + 1
+
+
 def main(filename):
     with open(filename) as f:
         lines = f.readlines()
 
-    # TODO implement me
+    stones = [int(stone) for stone in lines[0].strip().split()]
 
-    return None
+    for _ in range(BLINKS):
+        new_stones = []
+        for stone in stones:
+            if stone == 0:
+                new_stones.append(1)
+                continue
+            stone_digits = digits(stone)
+            if stone_digits % 2 == 0:
+                new_stones.append(stone // (10 ** (stone_digits // 2)))
+                new_stones.append(stone % (10 ** (stone_digits // 2)))
+                continue
+            new_stones.append(stone * 2024)
+
+        stones = new_stones
+
+    return len(stones)
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+BLINKS = 25
+EXPECTED_TEST_RESULT = 55312
 test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
--- 11/2.py
+++ 11/2.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import math
+from functools import cache
+
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
 
@@ -7,14 +10,38 @@ dirname = os.path.dirname(__file__)
 testfile = os.path.join(dirname, "test.txt")
 inputfile = os.path.join(dirname, "input.txt")
 
+
+def digits(x):
+    return int(math.log10(x)) + 1
+
+
+# two adjacent stones never interact, stone can only split into more stones
+# we can look at each stone independently and check into how many stones it will split in N blinks
+@cache
+def blink(stone, remaining_blinks):
+    if remaining_blinks == 0:
+        return 1
+    if stone == 0:
+        return blink(1, remaining_blinks - 1)
+    stone_digits = digits(stone)
+    if stone_digits % 2 == 0:
+        split = 10 ** (stone_digits // 2)
+        return blink(stone // split, remaining_blinks - 1) + blink(
+            stone % split, remaining_blinks - 1
+        )
+    return blink(stone * 2024, remaining_blinks - 1)
+
+
 def main(filename):
     with open(filename) as f:
         lines = f.readlines()
 
-    # TODO implement me
+    stones = [int(stone) for stone in lines[0].strip().split()]
 
-    return None
+    return sum([blink(stone, BLINKS) for stone in stones])
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+BLINKS = 75
+
+EXPECTED_TEST_RESULT = 65601038650482
 test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
