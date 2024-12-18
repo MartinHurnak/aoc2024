e50c67933243d2e4c037bf212904aad350853256
MartinHurnak <hurnak.m@gmail.com>
2024-12-18 19:18:36 +0100

    Day 17 Part 2

--- 17/2.py
+++ 17/2.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import re
+import copy
+import time
 
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
@@ -9,27 +11,31 @@ dirname = os.path.dirname(__file__)
 testfile = os.path.join(dirname, "test_part2.txt")
 inputfile = os.path.join(dirname, "input.txt")
 
+
 class LoopError(Exception):
     pass
+
+
 class Memory:
     def __init__(self, a, b, c):
         self.register_a = a
+        self.original_a = a
+        self.original_bits = 10
+        self.bits = self.original_bits
         self.register_b = b
         self.register_c = c
         self.instruction_pointer = 0
         self.output = []
-        self.output_pointer = 0
-        self.executed_jumps = set()
 
     def jump(self, operand):
         self.instruction_pointer = operand
-        jump_key = (self.register_a, self.register_b, self.register_c, self.instruction_pointer, self.output_pointer)
-        if jump_key in self.executed_jumps:
-            raise LoopError()
-        self.executed_jumps.add(jump_key)
 
-
-
+    def shift_a(self, i):
+        self.register_a = int(
+            f"{i:>03b}{self.register_a:>0{self.original_bits - 3}b}", 2
+        )
+        self.original_a = int(f"{i:>03b}{self.original_a:>0{self.bits}b}", 2)
+        self.bits += 3
 
     def combo_operand(self, operand):
         if operand in [0, 1, 2, 3]:
@@ -77,7 +83,6 @@ class Bst(Instruction):
         memory.increment_instruction_pointer()
 
 
-
 class Jnz(Instruction):
     OPCODE = 3
 
@@ -94,6 +99,7 @@ class Bxc(Instruction):
     def execute(self, operand, memory):
         memory.register_b = memory.register_b ^ memory.register_c
         memory.increment_instruction_pointer()
+
 
 class Out(Instruction):
     OPCODE = 5
@@ -122,13 +128,8 @@ class Cdv(Instruction):
 
 
 class Program:
-    def __init__(self, data):
-        m = re.match(
-            r"Register A: ([0-9]+)\nRegister B: ([0-9]+)\nRegister C: ([0-9]+)\n\nProgram: ([0-7,]*)",
-            data,
-        )
-        self.original_memory: Memory = Memory(*(int(value) for value in m.groups()[:3]))
-        self.program = [int(i) for i in m.groups()[3].split(",")]
+    def __init__(self, program):
+        self.program = program
         self.instruction_set = {
             inst.OPCODE: inst for inst in [Adv, Bxl, Bst, Jnz, Bxc, Out, Bdv, Cdv]
         }
@@ -138,33 +139,46 @@ class Program:
         operand = self.program[memory.instruction_pointer + 1]
         instruction.execute(operand, memory)
 
+    def execute(self, memory, output_pointer, stack):
 
-    def execute(self):
-        expected_output = self.program
-        output_pointer = 0
-        valid_states = []
-        for reg_a in range(128):
-            memory = Memory(
-                reg_a, self.original_memory.register_b, self.original_memory.register_c
-            )
-            try:
-                while memory.instruction_pointer + 1 < len(self.program) and output_pointer >= len(memory.output):
-                    self.execute_next(memory)
-                if self.program[0] == memory.output[output_pointer]:
-                    valid_states.append((reg_a, memory))
-            except LoopError:
-                pass
-        print(valid_states)
-        return 117440
-            
+        while memory.instruction_pointer + 1 < len(
+            self.program
+        ) and output_pointer >= len(memory.output):
+            self.execute_next(memory)
+        if memory.output == self.program and memory.instruction_pointer >= len(
+            self.program
+        ):
+            return [memory.original_a]
+        if output_pointer >= len(memory.output):
+            return None
+        if memory.instruction_pointer >= len(self.program):
+            return None
+        if self.program[: output_pointer + 1] == memory.output[: output_pointer + 1]:
+            results = []
+            for i in range(8):
+                new_memory = copy.deepcopy(memory)
+                new_memory.shift_a(i)
+                result = self.execute(new_memory, output_pointer + 1, stack)
+                if result:
+                    results += result
+            return results
 
 
 def main(filename):
     with open(filename) as f:
         data = f.read()
-
-    program = Program(data)
-    return program.execute()
+        m = re.match(
+            r"Register A: ([0-9]+)\nRegister B: ([0-9]+)\nRegister C: ([0-9]+)\n\nProgram: ([0-7,]*)",
+            data,
+        )
+    original_memory: Memory = Memory(*(int(value) for value in m.groups()[:3]))
+    program = [int(i) for i in m.groups()[3].split(",")]
+    program = Program(program)
+    for reg_a in range(1024):
+        memory = Memory(reg_a, original_memory.register_b, original_memory.register_c)
+        results = program.execute(memory, 0, [reg_a])
+        if results:
+            return min(results)
 
 
 EXPECTED_TEST_RESULT = 117440
