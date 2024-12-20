acf46c7df2abce1a4d537944c02e8cfedd8fe27b
MartinHurnak <hurnak.m@gmail.com>
2024-12-20 19:23:08 +0100

    Day 20

--- 20/1.py
+++ 20/1.py
@@ -1,5 +1,8 @@
 import os
 import sys
+from collections import deque
+import numpy as np
+
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
 
@@ -7,14 +10,65 @@ dirname = os.path.dirname(__file__)
 testfile = os.path.join(dirname, "test.txt")
 inputfile = os.path.join(dirname, "input.txt")
 
-def main(filename):
+
+def in_map(map, y, x):
+    return 0 <= y < len(map) and 0 <= x < len(map[y])
+
+
+def bfs(map, start, end):
+    queue = deque()
+    queue.append(end)
+    lengths = np.full_like(map, np.inf, dtype=np.float64)
+    lengths[end] = 0
+    while queue:
+        y, x = queue.popleft()
+        for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            if (
+                in_map(map, y + dy, x + dx)
+                and map[y + dy][x + dx]
+                and np.isinf(lengths[y + dy][x + dx])
+            ):
+                lengths[y + dy][x + dx] = lengths[y][x] + 1
+                queue.append((y + dy, x + dx))
+    return lengths
+
+
+def main(filename, min_cheat):
     with open(filename) as f:
         lines = f.readlines()
+    lines = [list(line.strip()) for line in lines]
+    map = np.zeros_like(lines, dtype=np.int64)
+    for y, row in enumerate(lines):
+        for x, col in enumerate(row):
+            if col == "#":
+                map[y][x] = False
+                continue
+            if col == "S":
+                start = (y, x)
+            elif col == "E":
+                end = (y, x)
+            map[y][x] = True
+    pathmap = bfs(map, start, end)
+    print(pathmap)
 
-    # TODO implement me
+    vertical_cheats = np.abs(pathmap[:-2] - pathmap[2:]) - 2
+    horizontal_cheats = np.abs(pathmap[:, :-2] - pathmap[:, 2:]) - 2
 
-    return None
+    horizontal_best = horizontal_cheats[
+        (horizontal_cheats >= min_cheat) & ~np.isinf(horizontal_cheats)
+    ]
+    vertical_best = vertical_cheats[
+        (vertical_cheats >= min_cheat) & ~np.isinf(vertical_cheats)
+    ]
+    return len(horizontal_best) + len(vertical_best)
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
-test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
+EXPECTED_TEST_RESULT = 4
+test_and_run(
+    main,
+    testfile,
+    EXPECTED_TEST_RESULT,
+    inputfile,
+    test_parameters=(36,),
+    main_paramenters=(100,),
+)
--- 20/2.py
+++ 20/2.py
@@ -1,5 +1,8 @@
 import os
 import sys
+from collections import deque
+import numpy as np
+
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
 
@@ -7,14 +10,83 @@ dirname = os.path.dirname(__file__)
 testfile = os.path.join(dirname, "test.txt")
 inputfile = os.path.join(dirname, "input.txt")
 
-def main(filename):
+
+def in_map(map, y, x):
+    return 0 <= y < len(map) and 0 <= x < len(map[y])
+
+
+def bfs(map, start, maxdepth=None):
+    queue = deque()
+    queue.append(start)
+    distances = np.full_like(map, np.inf, dtype=np.float64)
+    distances[start] = 0
+    while queue:
+        y, x = queue.popleft()
+        if maxdepth and distances[(y, x)] >= maxdepth:
+            break
+        for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            if (
+                in_map(map, y + dy, x + dx)
+                and map[y + dy][x + dx]
+                and np.isinf(distances[y + dy][x + dx])
+            ):
+                distances[(y + dy, x + dx)] = distances[(y, x)] + 1
+                queue.append((y + dy, x + dx))
+    return distances
+
+
+MAX_CHEAT_DIST = 20
+
+
+def main(filename, min_cheat):
     with open(filename) as f:
         lines = f.readlines()
+    lines = [list(line.strip()) for line in lines]
+    map = np.zeros_like(lines, dtype=np.bool)
+    for y, row in enumerate(lines):
+        for x, col in enumerate(row):
+            if col == "#":
+                map[y][x] = False
+                continue
+            if col == "S":
+                start = (y, x)
+            elif col == "E":
+                end = (y, x)
+            map[y][x] = True
+    track_points = bfs(map, end)
+    track_points = np.where(map, track_points, np.inf)
 
-    # TODO implement me
+    cheats = 0
+    manhattan_distances = np.sum(
+        np.abs(np.indices((2 * MAX_CHEAT_DIST + 1, 2 * MAX_CHEAT_DIST + 1)) - MAX_CHEAT_DIST), axis=0
+    )
+    manhattan_distances = np.where(
+        manhattan_distances > MAX_CHEAT_DIST, np.inf, manhattan_distances
+    )
 
-    return None
+    for trackpoint in np.argwhere(map):
+        y, x = trackpoint = tuple(trackpoint)
+        selection = track_points[
+            max(0, y - MAX_CHEAT_DIST) : y + MAX_CHEAT_DIST + 1,
+            max(0, x - MAX_CHEAT_DIST) : x + MAX_CHEAT_DIST + 1,
+        ]
+        cheat_distances = manhattan_distances[
+            max(0, MAX_CHEAT_DIST - y) : MAX_CHEAT_DIST + min(map.shape[0] - y, MAX_CHEAT_DIST + 1),
+            max(0, MAX_CHEAT_DIST - x) : MAX_CHEAT_DIST + min(map.shape[1] - x, MAX_CHEAT_DIST + 1),
+        ]
+        possible_cheats = track_points[y, x] - (selection + cheat_distances)
+
+        cheats += np.sum(possible_cheats >= min_cheat)
+
+    return cheats
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
-test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
+EXPECTED_TEST_RESULT = 285
+test_and_run(
+    main,
+    testfile,
+    EXPECTED_TEST_RESULT,
+    inputfile,
+    test_parameters=(50,),
+    main_paramenters=(100,),
+)
