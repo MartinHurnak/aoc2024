ebff38ee5ab2be75066c61ca38ba2c1d093e39ad
MartinHurnak <hurnak.m@gmail.com>
2024-12-22 11:21:25 +0100

    Day 22

--- 22/1.py
+++ 22/1.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
 
@@ -7,14 +8,35 @@ dirname = os.path.dirname(__file__)
 testfile = os.path.join(dirname, "test.txt")
 inputfile = os.path.join(dirname, "input.txt")
 
+
+def mix(secret, number):
+    return secret ^ number
+
+
+def prune(secret):
+    return secret % 16777216
+
+
+def next(secret):
+    secret = prune(mix(secret, secret * 64))
+    secret = prune(mix(secret, secret // 32))
+    secret = prune(mix(secret, secret * 2048))
+    return secret
+
+
 def main(filename):
     with open(filename) as f:
         lines = f.readlines()
 
-    # TODO implement me
+    sum = 0
+    for line in lines:
+        secret = int(line)
+        for _ in range(2000):
+            secret = next(secret)
+        sum += secret
 
-    return None
+    return sum
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+EXPECTED_TEST_RESULT = 37327623
 test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
--- 22/2.py
+++ 22/2.py
@@ -1,20 +1,56 @@
 import os
 import sys
+import numpy as np
+from collections import Counter
+
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
 
 dirname = os.path.dirname(__file__)
-testfile = os.path.join(dirname, "test.txt")
+testfile = os.path.join(dirname, "test2.txt")
 inputfile = os.path.join(dirname, "input.txt")
+
+
+def mix(secret, number):
+    return secret ^ number
+
+
+def prune(secret):
+    return secret % 16777216
+
+
+def next(secret):
+    secret = prune(mix(secret, secret * 64))
+    secret = prune(mix(secret, secret // 32))
+    secret = prune(mix(secret, secret * 2048))
+    return secret
+
 
 def main(filename):
     with open(filename) as f:
         lines = f.readlines()
 
-    # TODO implement me
+    costs = []
+    for l, line in enumerate(lines):
+        secret = int(line)
+        costs.append([secret % 10])
+        for _ in range(2000):
+            secret = next(secret)
+            costs[l].append(secret % 10)
 
-    return None
+    cost_diffs = np.diff(np.array(costs))
+
+    sequence_costs = Counter()
+    for r, row in enumerate(cost_diffs):
+        row_costs = Counter()
+        for i in range(len(row) - 3):
+            seq = tuple(row[i : i + 4])
+            if seq not in row_costs:
+                row_costs[seq] = costs[r][i + 4]
+        sequence_costs.update(row_costs)
+
+    return sequence_costs.most_common(n=1)[0][1]
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+EXPECTED_TEST_RESULT = 23
 test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
