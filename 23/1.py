36061ec26fe3d30222a49d0d5dff90d06ef00a90
MartinHurnak <hurnak.m@gmail.com>
2024-12-23 22:00:24 +0100

    Day 23

--- 23/1.py
+++ 23/1.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
 
@@ -7,14 +8,27 @@ dirname = os.path.dirname(__file__)
 testfile = os.path.join(dirname, "test.txt")
 inputfile = os.path.join(dirname, "input.txt")
 
+
 def main(filename):
     with open(filename) as f:
         lines = f.readlines()
 
-    # TODO implement me
+    connections = {}
+    for line in lines:
+        c1, c2 = line.strip().split("-")
+        connections.setdefault(c1, set()).add(c2)
+        connections.setdefault(c2, set()).add(c1)
 
-    return None
+    triplets = set()
+    for c1 in connections.keys():
+        if not c1.startswith("t"):
+            continue
+        for c2 in connections[c1]:
+            for c3 in connections[c1] & connections[c2]:
+                triplets.add(tuple(sorted((c1, c2, c3))))
+
+    return len(triplets)
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+EXPECTED_TEST_RESULT = 7
 test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
--- 23/2.py
+++ 23/2.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import numpy as np
+
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
 
@@ -7,14 +9,35 @@ dirname = os.path.dirname(__file__)
 testfile = os.path.join(dirname, "test.txt")
 inputfile = os.path.join(dirname, "input.txt")
 
+
+# https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+def bron_kerbosh(connections, r, p, x):
+    cliques = set()
+    if not p and not x:
+        return {tuple(sorted(r))}
+    for v in p:
+        cliques |= bron_kerbosh(
+            connections, r | set([v]), p & connections[v], x & connections[v]
+        )
+        p = p - set([v])
+        x = x | set([v])
+    return cliques
+
+
 def main(filename):
     with open(filename) as f:
         lines = f.readlines()
 
-    # TODO implement me
+    connections = {}
+    for line in lines:
+        c1, c2 = line.strip().split("-")
+        connections.setdefault(c1, set()).add(c2)
+        connections.setdefault(c2, set()).add(c1)
 
-    return None
+    cliques = bron_kerbosh(connections, set(), set(connections.keys()), set())
+    best_clique = sorted(cliques, key=lambda x: -len(x))[0]
+    return ",".join(best_clique)
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+EXPECTED_TEST_RESULT = "co,de,ka,ta"
 test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
