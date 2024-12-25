7d8c4a442e567e53450b8e82bcbeb690ae4137f8
MartinHurnak <hurnak.m@gmail.com>
2024-12-25 20:18:17 +0100

    Day 25 Part 1

--- 25/1.py
+++ 25/1.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import numpy as np
+
 sys.path.insert(0, os.getcwd())
 from test_run import test_and_run
 
@@ -7,14 +9,32 @@ dirname = os.path.dirname(__file__)
 testfile = os.path.join(dirname, "test.txt")
 inputfile = os.path.join(dirname, "input.txt")
 
+
 def main(filename):
     with open(filename) as f:
-        lines = f.readlines()
+        data = f.read()
 
-    # TODO implement me
+    locks, keys = [], []
+    for entry in data.split("\n\n"):
+        heights = np.zeros((5,))
+        for line in entry.split("\n"):
+            for i, col in enumerate(line):
+                if col == "#":
+                    heights[i] += 1
+        heights -= 1
+        if entry.startswith("#####\n"):
+            locks.append(heights)
+        else:
+            keys.append(heights)
 
-    return None
+    sum = 0
+    for lock in locks:
+        for key in keys:
+            if ((key + lock) <= 5).all():
+                sum += 1
+
+    return sum
 
 
-EXPECTED_TEST_RESULT = 0  # TODO change based on test
+EXPECTED_TEST_RESULT = 3
 test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
